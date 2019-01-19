76c58be8f9151a7ca38dbfab2967374a8ff06e76
lesteven <le_steven@outlook.com>
2019-01-18 20:21:23 -0800

    update style

--- src/common/views/about/data.js
+++ src/common/views/about/data.js
@@ -3,7 +3,6 @@ const data = [
   "Hey, thanks for visiting my site!",
   "I have been studying CS (computer science) for the past couple of years on my own; the subjects that I studied include math, algorithms, CS theory and web development.",
   "Besides CS, I like to read, work out and play guitar during my free time. Although most of the time, I spend my time learning and trying to catch up.",
-  "If you would like to get in touch, you could reach me at my email Le_steven@outlook.com."
 ]
 
 export default data;
