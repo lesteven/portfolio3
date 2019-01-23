ec26dd77f1188c90870ef97941ef92ab9f60e48b
lesteven <le_steven@outlook.com>
2019-01-22 16:21:43 -0800

    update font

--- src/common/views/about/data.js
+++ src/common/views/about/data.js
@@ -3,7 +3,6 @@ const data = [
   "Hey, thanks for visiting!",
   "I studied something completely different in college, but then I decided it was not the path I wanted to take anymore. So a couple years ago, I made the leap towards CS instead. My learning has mostly been done through online lectures, books and projects. Although it was somewhat difficult at times, overall it was a great experience.",
 "With the knowledge that I acquired, I am ready for the next step and I am looking for new opportunities to learn and grow as a developer!",
-  "Yes, that's right, I'm looking for work.",
   "I mostly do web development work at the moment, but I am also learning android development as well."
 ]
 
